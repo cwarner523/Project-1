8146d97fc1c26edf178dc2a9e9c4f903f04cf07d
Carly Warner <cwarner523>
2017-07-12 10:36:05 -0400

    started js declaring variables

---
+++ main.js
@@ -0,0 +1,18 @@
+//declare all variables for circle divs
+$(function() {
+
+var $circle1Yellow = $('#circle1');
+var $circle2Red = $('#circle2');
+var $circle1Yellow = $('#circle1');
+var $circle3Blue = $('#circle3');
+var $circle4Green = $('#circle4');
+//declare var for start button
+var $startButton = $('#startgame');
+
+//start function
+$startButton.click(function()
+{
+    console.log("your game is starting");
+})
+
+});
