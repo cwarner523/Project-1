10097c8823557f19638166ab44e1c9331b729ea9
Carly Warner <cwarner523>
2017-07-13 11:35:19 -0400

    got random div to flash

--- main.js
+++ main.js
@@ -1,18 +1,54 @@
-//declare all variables for circle divs
-$(function() {
+// declaring variables for boxes
+var $circle1 = $('#circle1')
+var $circle2 = $('#circle2')
+var $circle3 = $('#circle3')
+var $circle4 = $('#circle4')
+//array of divs
+var arrOfDivs = [$circle1, $circle2, $circle3, $circle4];
 
-var $circle1Yellow = $('#circle1');
-var $circle2Red = $('#circle2');
-var $circle1Yellow = $('#circle1');
-var $circle3Blue = $('#circle3');
-var $circle4Green = $('#circle4');
-//declare var for start button
+// decarling empty arrays to store user and game patterns
+var gameTurn = [];
+var userTurn = [];
 var $startButton = $('#startgame');
 
-//start function
-$startButton.click(function()
-{
-    console.log("your game is starting");
-})
 
-});
+//start function
+function startGame()
+{
+    $startButton.click(function()
+    {
+        console.log("your game is starting");
+        randomizer();
+    })
+}
+
+function randomizer()
+{
+   gameTurn.push(getRandom());
+   userTurn = [];
+   console.log(gameTurn);
+   flasher(1);
+   userBoard()
+}
+
+function flasher(times)
+{
+    if (times > 0)
+    {
+        $(gameTurn[0]).fadeTo(350, 1.0, function() {$(this).fadeTo(250, 0.7); });
+    }
+}
+
+function userBoard()
+{
+
+}
+
+
+function getRandom()
+{
+    return arrOfDivs[Math.floor(Math.random() * 4)];
+}
+
+startGame();
+//randomizer();
