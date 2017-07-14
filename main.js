65180b74afb7e0efc66b9bcf4acc3caa58709812
Carly Warner <cwarner523>
2017-07-14 14:38:49 -0400

    got game logic and changing css

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 
 //array of divs
-const buttons = ["#circle1", "#circle2", "#circle3", "#circle4"];
+const $buttons = ["circle1", "circle2", "circle3", "circle4"];
 
 // decarling empty arrays to store user and game patterns
 var gameTurn = [];
 var userTurn = [];
 var $startButton = $('#startgame');
 var $circle = $('.circle')
+let currentCheckIndex = 0;
+// initialize button click events
+$circle.click(function()
+{
+    let userSelection = $(this).attr('id');
+    userTurn.push(userSelection);
+    console.log(userSelection)
+    flasher();  
 
+    checkForWin(); 
+});
+
+//declaring count variable
+var count = 0;
 
 //start function
 function startGame()
@@ -19,53 +32,69 @@ function startGame()
     })
 }
 
+
 function randomizer()
 {
     console.log('randomizer')
-   gameTurn.push(buttons[getRandom()]);          //build array from pattern
-   let randNum = gameTurn[0];
+   gameTurn.push($buttons[getRandom()]);          //build array from pattern
+   let randNum = gameTurn;
    console.log(randNum);
    flasher(1);                
-    activateUser();          //Players turn to build matching pattern
+// activateUser();          //Players turn to build matching pattern
 }
 
 function flasher(times)
 {
-    console.log('flasher')
-    if (times > 0)
+    for(i = 0; i < gameTurn.length; i++)
     {
-        $(gameTurn[0]).fadeTo(350, 1.0, function() {$(this).fadeTo(250, 0.7); });
+        $(`#${gameTurn[i]}`).fadeTo(450, 1.0, function() {$(this).fadeTo(350, 0.7); });
     }
 }
 
-
 function activateUser()
 {
-    console.log('activate user')
-    $circle.click(function()
-    {
-        let userSelection = $(this).attr('id');
-        userTurn.push(userSelection);
-        console.log(userSelection)
-        flasher(1);   
-    })
+    let userSelection = $(this).attr('id');
+    userTurn.push(userSelection);
+    console.log(userSelection)
+    flasher();  
+    checkForWin(); 
     //console.log($(this));
     //userTurn.push(userSelection);       //adding users selection to their pattern
     //flasher(1);                         //flash when user clicks
     //console.log(userTurn[0])
-    checkForWin();
+    // `#${gameTurn[0]}`
 }
 
 function checkForWin()
-{  
-    
-    if (randNum)
+{ 
+
+    console.log(`Game turn: ${gameTurn}, user turn: ${userTurn}`);
+    if (gameTurn[currentCheckIndex] === userTurn[currentCheckIndex])
     {
-        console.log("count 1")
-        randomizer();
+        console.log("count 1");
+        
     }else{
-        console.log("You have lost, try again!")
+    alert("You have lost, try again!");
+    location.reload();
+    }   
+    
+
+
+    console.log(currentCheckIndex);
+    console.log(gameTurn.length);
+    console.log(gameTurn.length-1);
+    $('#counter').text(count++);
+    if(currentCheckIndex ===  gameTurn.length-1){
+        console.log('resetting check index');
+        $('#counter').text(count++);
+        randomizer();
+        currentCheckIndex = 0;
+        userTurn = [];
     }
+    else{
+        currentCheckIndex++;
+    }
+
 }
 
 // create pattern
